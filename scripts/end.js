e25d2340ef04240b3e986ff1517c79edc237f865
ovokpus <ovo@live.ca>
2020-11-23 19:55:04 -0700

    updated code

--- scripts/end.js
+++ scripts/end.js
@@ -25,6 +25,6 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("highscores.html");
+    window.location.assign("hi-scores.html");
 
 };
