a385b4d2ef501926854d02af56595400930feca3
ovokpus <ovo@live.ca>
2020-12-15 21:13:25 +0100

    updates with dynamic functionality

--- scripts/hi-scores.js
+++
@@ -1,8 +0,0 @@
-const hiScoresList = document.querySelector("#hi-scores-list");
-const hiScores = JSON.parse(localStorage.getItem("hiScores")) || [];
-
-hiScoresList.innerHTML = hiScores
-.map( score => {
-        return `<li class="hi-scores">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
