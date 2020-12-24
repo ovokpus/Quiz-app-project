b6970a30cc589373ad6417e8b80e32030460d4da
ovokpus <ovo@live.ca>
2020-12-23 18:25:03 -0700

    removed static files

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const routes = require('./routes');
 const app = express();
 const port = 3000;
 
-app.set('views', './views');
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, './static')));
--- static/README.md
+++
@@ -1,3 +0,0 @@
-# Quiz-app-project(Static)
-
-### This is the static version of the Quiz App
